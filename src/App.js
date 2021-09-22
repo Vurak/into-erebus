f8fbc6810e4149a7e29d078dcb8900dfc0add933
Vurak <vurak.of.sulcan@gmail.com>
2021-09-23 00:13:38 +0100

    added body-container to contain main content and fixed some css causing issues in firefox scroll. removed comments

--- src/App.js
+++ src/App.js
@@ -4,50 +4,39 @@ import { Environment } from "@react-three/drei"
 import Particles from './components/particles/Particles';
 import Model from "./components/Models"
 import Overlay from "./components/Overlay"
+import { useState } from "react";
 
 function App() {
+  const [scrollProgress, setScrollProgress] = useState(0)
+
   const overlay = useRef()
   const caption = useRef()
   const scroll = useRef(0)
+
   return (
     <>
-      <Canvas
-        shadows
-        onCreated={(state) => state.events.connect(overlay.current)}
-        raycaster={{ computeOffsets: ({ clientX, clientY }) => ({ offsetX: clientX, offsetY: clientY }) }}>
-        <fog attach="fog" args={["black", 0.1, 50]} />
-        {/* <ambientLight intensity={1} /> */}
-         <Suspense fallback={null}>
-           <Model scroll={scroll} />
-           {/* <Environment preset="city" /> */}
-           {/* <Particles count={5000}/> */}
-         </Suspense>
-      </Canvas>
-      <Overlay ref={overlay} caption={caption} scroll={scroll} />
+      <div className="body-container">
+        <Canvas
+          shadows
+          onWheel={(e) => {
+            scroll.current = e.target.scrollTop / (e.target.scrollHeight - window.innerHeight)
+            caption.current.innerText = scroll.current.toFixed(2)
+            setScrollProgress(scroll.current)
+          }}
+          onCreated={(state) => state.events.connect(overlay.current)}
+          raycaster={{ computeOffsets: ({ clientX, clientY }) => ({ offsetX: clientX, offsetY: clientY }) }}>
+          <fog attach="fog" args={["black", 0.1, 50]} />
+          {/* <ambientLight intensity={1} /> */}
+          <Suspense fallback={null}>
+            <Model scroll={scroll} />
+            {/* <Environment preset="city" /> */}
+            {/* <Particles count={5000}/> */}
+          </Suspense>
+        </Canvas>
+        <Overlay ref={overlay} caption={caption} scroll={scroll} />
+      </div>
     </>
   );
 }
 
 export default App;
-
-
-// export default function App() {
-//   const overlay = useRef()
-//   const caption = useRef()
-//   const scroll = useRef(0)
-//   return (
-//     <>
-//       <Canvas
-//         shadows
-//         onCreated={(state) => state.events.connect(overlay.current)}
-//         raycaster={{ computeOffsets: ({ clientX, clientY }) => ({ offsetX: clientX, offsetY: clientY }) }}>
-//         <ambientLight intensity={1} />
-//         <Suspense fallback={null}>
-//           <Model scroll={scroll} />
-//           <Environment preset="city" />
-//         </Suspense>
-//       </Canvas>
-//       <Overlay ref={overlay} caption={caption} scroll={scroll} />
-//     </>
-//   )
-// }
