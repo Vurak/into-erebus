d2bbc9dbb3a60423891ba447646ebe8f462f42b2
Vurak <vurak.of.sulcan@gmail.com>
2021-10-05 20:18:33 +0100

    exapanded scoll sections component to use scrollProgress from context

--- src/components/ScrollSections.js
+++ src/components/ScrollSections.js
@@ -1,9 +1,19 @@
+import { useContext, useEffect } from "react"
 import NormalizedScrollSection from "./scroll/NormalizedScrollSection"
+import ScrollContext from "../context/ScrollContext"
 
-const ScrollSections = ({scroll}) => {
+
+const ScrollSections = () => {
+  
+  const { scrollProgress } = useContext(ScrollContext)
+  
+  useEffect(() => {
+    console.log('s',scrollProgress)
+  })
+
   return (
     <>
-      <NormalizedScrollSection scroll={scroll} start={0} end={0.1}/>
+      <NormalizedScrollSection scroll={scrollProgress} start={0} end={0.1}/>
     </>
   )
 }
