18507b38eb8e9cecf8a48f26025fc209fd843131
Vurak <vurak.of.sulcan@gmail.com>
2021-10-18 23:39:13 +0100

    created NormalizedScrollGroup which will lerp between position and rotation according to its own slice of scroll progress set in props

---
+++ src/components/scroll/NormalizedScrollGroup.js
@@ -0,0 +1,37 @@
+import { useFrame } from "@react-three/fiber"
+import { useContext, useEffect, useState } from "react"
+import ScrollContext from "../../context/ScrollContext"
+
+const lerpVector3 = (initial, final, x) => {
+  const current = initial
+  for (let i = 0; i < 3; i++) {
+    current[i] += final[i] * x
+  }
+  return current
+}
+
+export const NormalizedScrollGroup = ({start, end, position=[0,0,0], deltaPosition=[0,0,0], rotation=[0,0,0], deltaRotation=[0,0,0], children}) => {
+  const [pos, setPos] = useState(position)
+  const [rot, setRot] = useState(rotation)
+  const [normalizedScroll, setNormalizedScroll] = useState(0)
+  const { scrollProgress, setScrollProgress } = useContext(ScrollContext)
+  
+  useFrame(() => {
+    const nScroll = scrollProgress/(end - start)
+    if (nScroll != normalizedScroll) {
+      setNormalizedScroll(nScroll)
+      setPos(lerpVector3(position, deltaPosition, normalizedScroll))
+      setRot(lerpVector3(rotation, deltaRotation, normalizedScroll))
+    }
+  })
+
+  useEffect(() => {
+    console.log("asaa", scrollProgress)
+  }, [scrollProgress])
+
+  return(
+    <group position={pos} rotation={rot}>
+      {children}
+    </group>
+  )
+}
