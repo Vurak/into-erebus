205d9e6af72352d2437ab55a9b51e052d36f0c78
Vurak <vurak.of.sulcan@gmail.com>
2021-11-04 23:01:12 +0000

    passing TargetContext through Canvas

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -4,32 +4,29 @@ import ScrollSections from "./ScrollSections";
 import ScrollCamera from "./scroll/ScrollCamera"
 
 import ScrollContext from "../context/ScrollContext";
+import TargetContext from "../context/TargetContext";
 
 const CanvasWrapper = ({overlay}) => {
-  const value = useContext(ScrollContext)
-
+  const value_scroll = useContext(ScrollContext)
+  const value_target = useContext(TargetContext)
+  
   return (
     <Canvas
       shadows
-      onWheel={(e) => {
-        // setScrollProgress(e.target.scrollTop / (e.target.scrollHeight - window.innerHeight))
-        // scroll.current = e.target.scrollTop / (e.target.scrollHeight - window.innerHeight)
-        // caption.current.innerText = scroll.current.toFixed(2)
-        // console.log(e.target.scrollTop / (e.target.scrollHeight - window.innerHeight))
-        // value.setScrollProgress(e.target.scrollTop / (e.target.scrollHeight - window.innerHeight))
-      }}
       onCreated={(state) => state.events.connect(overlay.current)}
       raycaster={{ computeOffsets: ({ clientX, clientY }) => ({ offsetX: clientX, offsetY: clientY }) }}>
       <fog attach="fog" args={["black", 25, 50]} />
       <color attach="background" args={["black"]} />
       {/* <ambientLight intensity={1} /> */}
       <Suspense fallback={null}>
-        <ScrollContext.Provider value={value}>
-          <ScrollCamera />
-          <ScrollSections />
-          {/* <Model scroll={scroll} /> */}
-          {/* <Environment preset="city" /> */}
-          {/* <Particles count={5000}/> */}
+        <ScrollContext.Provider value={value_scroll}>
+          <TargetContext.Provider value={value_target}>
+            <ScrollCamera />
+            <ScrollSections />
+            {/* <Model scroll={scroll} /> */}
+            {/* <Environment preset="city" /> */}
+            {/* <Particles count={5000}/> */}
+          </TargetContext.Provider>
         </ScrollContext.Provider>
       </Suspense>
     </Canvas>
