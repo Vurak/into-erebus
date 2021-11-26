34bf1694e786ed65f158ff4260328c544c00309e
Vurak <vurak.of.sulcan@gmail.com>
2021-11-26 22:16:15 +0000

    uninstalled rtailwindcss and set up craco config

--- craco.config.js
+++ craco.config.js
@@ -1,10 +1,17 @@
+// craco.config.js
+const path = require(`path`);
+
 module.exports = {
   style: {
     postcss: {
       plugins: [
-        require('tailwindcss'),
         require('autoprefixer'),
       ],
     },
+  },
+  webpack: {
+    alias: {
+      '@images': path.resolve(__dirname, './src/images'),
+    }
   },
 }
