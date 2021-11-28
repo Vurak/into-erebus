1440dcfac3a270d2fb5f3db07e424b61aff5030d
Vurak <vurak.of.sulcan@gmail.com>
2021-11-28 14:42:40 +0000

    adjusted cursor animations and click evenmt listeners

--- src/components/Cursor.js
+++ src/components/Cursor.js
@@ -1,30 +1,37 @@
-import { useState } from "react"
-import { useEffect, useRef } from "react"
+import { useState, useEffect, useRef } from "react"
 
 import './Cursor.css'
 
 const Cursor = () => {
   const cursor = useRef(null)
-
+  const [click, setClick] = useState(false)
   const moveCursor = (e) => {
     cursor.current.setAttribute('style',`transform: translate3d(${e.clientX-20}px, ${e.clientY-window.innerHeight-20}px, 0);`)
+  }
+
+  const clickCursor = () => {
+    // if (click) return
+    setClick(true)
+    setTimeout(() => setClick(false), 500)
   }
   
   useEffect(() => {
     if (!cursor?.current) return
     window.addEventListener('mousemove', moveCursor)
-
+    window.addEventListener('mousedown', clickCursor)
     return () => {
       window.removeEventListener('mousemove', moveCursor)
+      window.removeEventListener('mousedown', clickCursor)
     }
   }, [])
 
   return (
     <div
     ref={cursor}
-    className="cursor"
-    // style={{top: `${cursorPos[0]}px`, left: `${cursorPos[1]}px`}}
-    />
+    className={`cursor ${click && 'cursor-click'}`}
+    >
+      <div className={`cursor-center ${click && 'cursor-center-click'}`}/>
+    </div>
   )
 
 }
