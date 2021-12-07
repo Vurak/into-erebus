078fe6656f945e131d26728d2b3edcfee8c19bf3
Vurak <vurak.of.sulcan@gmail.com>
2021-12-07 22:57:55 +0000

    introduction setup of animations with react-transition-group. still very much in the works

--- src/components/overlay/Introduction.js
+++ src/components/overlay/Introduction.js
@@ -2,40 +2,54 @@ import { useState } from 'react'
 import { useCursor } from '../hooks'
 import { ArrowRight, ChevronUp, ChevronDown } from '@icons'
 import './introduction.css'
+import { TransitionGroup, CSSTransition } from 'react-transition-group'
 
 const content = [
   {
+    id: 1,
     text: "Into Erebus",
     title: true
   },
   {
+    id: 2,
     text: "For a while it had been thought there might be a route to the orient over the Americas, via the Arctic. In 1745 the British Admiralty promised a £20,000 prize for whoever discovered this passage."
+  },
+  {
+    id: 3,
+    text: "asdasdasdasda. In 1745 the British Admiralty promised a £20,000 prize for whoever discovered this passage."
   }
 ]
 
 const Slide = ({s}) => {
-  return(
-    <p className={`${content[s].title ? 'intro-title' : ''}`}>
-      {content[s].text}
+  return(    
+    <p className={`${s.title ? 'intro-title' : ''}`}>
+      {s.text}
     </p>
   )
 }
 
 export const Introduction = () => {
   const [slide, setSlide] = useState(0)
+  const [slideAnimation, setSlideAnimation] = useState(null)
   const { setClickable } = useCursor()
 
   const handleClick = (add=true) => {
-    setSlide(s => {
-      console.log(s)
-      if (add && s < content.length-1) return s+1
-      else if (!add && s > 0) return s-1
-      return s
-    })
+    setSlideAnimation(add ? 'out' : 'in')
+    setTimeout(() => {
+      // setSlideAnimation(add ? 'in' : 'out')
+      // setSlideAnimation(null)
+      setSlide(s => {
+        console.log(s)
+        if (add && s < content.length-1) return s+1
+        else if (!add && s > 0) return s-1
+        return s
+      })
+    }, 500)
   }
 
   return (
     <div className="intro-container">
+      
       <div
         className="slide-arrow slide-arrow-top"
         onMouseEnter={() => setClickable(true)}
@@ -43,13 +57,24 @@ export const Introduction = () => {
         onClick={() => handleClick(false)}>
         <ChevronUp/>
       </div>
-      <div
-        className="intro-text"
+      <TransitionGroup>
+        <CSSTransition
+          key={content[slide].id}
+          in
+          timeout={1000}
+          classNames="slide"
+          className="intro-text">
+          <Slide s={content[slide]}/>
+        </CSSTransition>
+      </TransitionGroup>
+      
+      {/* <div
+        className={`slide-content ${slideAnimation ? (slideAnimation == 'in' ? 'slide-enter-top' : 'slide-enter-bot') : ''}`}
         onClick={handleClick}
         onMouseEnter={() => setClickable(true)}
         onMouseLeave={() => setClickable(false)}>
         <Slide s={slide}/>
-      </div>
+      </div> */}
       <div
         className="slide-arrow slide-arrow-bot"
         onMouseEnter={() => setClickable(true)}
