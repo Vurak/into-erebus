67e9b8f579aa1577c38dda9af594cbdafad10882
Vurak <vurak.of.sulcan@gmail.com>
2021-12-19 13:22:43 +0100

    set up font preloading in canvas component which will setLoading to false once all fonts have been loaded

--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -1,20 +1,58 @@
-import React, { Suspense, useContext, useRef } from "react"
+import React, { Suspense, useContext, useEffect, useRef } from "react"
 import { Canvas } from '@react-three/fiber';
 import ScrollSections from "./ScrollSections";
 import ScrollCamera from "./scroll/ScrollCamera"
 
 import ScrollContext from "../context/ScrollContext";
 import TargetContext from "../context/TargetContext";
+import { Introduction } from "./overlay/Introduction";
+import { preloadFont } from "troika-three-text";
 
-const CanvasWrapper = ({overlay}) => {
+import {
+  yujiboku,
+  imfell
+} from '@fonts'
+
+let fontsLoaded = 0
+let requiredFonts = 2
+
+const CanvasWrapper = ({ overlay, setLoading }) => {
   const value_scroll = useContext(ScrollContext)
   const value_target = useContext(TargetContext)
   
+  const checkLoadedFonts = () => {
+    console.log(fontsLoaded)
+    if (fontsLoaded >= requiredFonts)
+      setLoading(false)
+  }
+  
+  // Load fonts
+  useEffect(() => {
+    preloadFont({
+      font: yujiboku,
+      characters: 'abcdefghijklmnopqrstuvwxyz',
+    }, () => {
+      console.log("preloaded yujiboku")
+      fontsLoaded++
+      checkLoadedFonts()
+    })
+
+    preloadFont({
+      font: imfell,
+      characters: 'abcdefghijklmnopqrstuvwxyz',
+    }, () => {
+      console.log("preloaded imfell")
+      fontsLoaded++
+      checkLoadedFonts()
+    })
+  }, [])
+
   return (
     <Canvas
       shadows
       onCreated={(state) => state.events.connect(overlay.current)}
-      raycaster={{ computeOffsets: ({ clientX, clientY }) => ({ offsetX: clientX, offsetY: clientY }) }}>
+      raycaster={{ computeOffsets: ({ clientX, clientY }) => ({ offsetX: clientX, offsetY: clientY }) }}
+      onLoad={() => cosole.log("loaded scene")}>
       <fog attach="fog" args={["black", value_target.target ? 10 : 25, value_target.target ? 20 : 50]} />
       <color attach="background" args={["black"]} />
       {/* <ambientLight intensity={1} /> */}
--- src/fonts/index.js
+++ src/fonts/index.js
@@ -1,7 +1,11 @@
 import imfell_pica from './imfell-pica.ttf'
+import imfell_pica_json from './imfell-pica-json.json'
 import imfell from './imfell.ttf'
+import yujiboku from './yujiboku.ttf'
 
 export {
   imfell_pica,
-  imfell
+  imfell_pica_json,
+  imfell,
+  yujiboku
 }
