9d700c291adf50c46de19385659809a3ea2781cc
Vurak <vurak.of.sulcan@gmail.com>
2021-12-24 18:00:51 +0100

    removed garbage from OffsetScrollSection

--- src/components/scroll/OffsetScrollSection.js
+++ src/components/scroll/OffsetScrollSection.js
@@ -1,9 +1,4 @@
-import { Plane } from "@react-three/drei"
-import { useContext, useEffect, useState } from "react"
-import DepthContext from "../../context/DepthContext"
-
 const OffsetScrollSection = ({offset, scrollRange, children}) => {
-  const depth = useContext(DepthContext)
   return (
     <group position={[offset[0],offset[1],-offset[2]]}>
       {children}
