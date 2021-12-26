ccd7fc78376e07636410e9e534540a0728eeeb51
Vurak <vurak.of.sulcan@gmail.com>
2021-12-26 18:49:18 +0100

    added rotation and positioning as camera approaches for beechey island map

--- src/components/sections/Beechey.js
+++ src/components/sections/Beechey.js
@@ -3,6 +3,7 @@ import { useEffect } from "react"
 import {
   BeecheyMap
 } from '../../models'
+import { NormalizedScrollGroup } from "../scroll/NormalizedScrollGroup"
 
 export const Beechey = ({ z }) => {
   const { nodes, materials } = useGLTF(BeecheyMap)
@@ -13,23 +14,25 @@ export const Beechey = ({ z }) => {
 
   return (
     <group position={[0,0,-z]} rotation={[Math.PI/2,0,0]} scale={[5,5,5]}>
-      <mesh
+        <NormalizedScrollGroup start={0.47} end={0.5} deltaPosition={[0.2,-0.2,0.05]} lerpFactor={0.1} deltaRotation={[-Math.PI/2.8, -Math.PI/2, Math.PI/8]}>
+          <mesh
 
-        geometry={nodes.Plane.geometry}
-        material={materials['Material.002']}
-      />
-      <mesh
-        geometry={nodes['Cube004'].geometry}
-        material={materials['graves']}
-      />
-      <mesh
-        geometry={nodes['Cube005'].geometry}
-        material={materials['graves']}
-      />
-      <mesh
-        geometry={nodes['Cube006'].geometry}
-        material={materials['graves']}
-      />
+            geometry={nodes.Plane.geometry}
+            material={materials['Material.002']}
+          />
+          <mesh
+            geometry={nodes['Cube004'].geometry}
+            material={materials['graves']}
+          />
+          <mesh
+            geometry={nodes['Cube005'].geometry}
+            material={materials['graves']}
+          />
+          <mesh
+            geometry={nodes['Cube006'].geometry}
+            material={materials['graves']}
+          />
+        </NormalizedScrollGroup>
     </group>
   )
 }
